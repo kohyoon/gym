ec88dc770e8549da65380004e26e8169df5e0d2d
kohyoon <piyoong4@gmail.com>
2025-07-24 16:47:36 +0900

    feat: MembershipRefundLog에 @DateTimeFormat 어노테이션 추가

--- src/main/java/com/project/gym/domain/MembershipRefundLog.java
+++ src/main/java/com/project/gym/domain/MembershipRefundLog.java
@@ -1,18 +1,18 @@
 package com.project.gym.domain;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Data
 public class MembershipRefundLog {
-
     private Long logId;
     private Long refundId;
 
     private Integer actionType; // 0: 요청, 1: 승인, 2: 거절, 3: 시스템, 9: 기타
     private String actionDetail;
     private String actionBy;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate actionAt;
-
 }
