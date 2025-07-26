6dce55b6d086794753abd7f72f003edcfd928572
kohyoon <piyoong4@gmail.com>
2025-07-26 22:09:16 +0900

    feat: AdminMapper에 관리자 등록, 중복 확인, 목록 조회 메서드 추가

---
+++ src/main/java/com/project/gym/mapper/AdminMapper.java
@@ -0,0 +1,19 @@
+package com.project.gym.mapper;
+
+import com.project.gym.domain.Admin;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface AdminMapper {
+
+    // 회원 추가 (가입)
+    void insertAdmin(Admin admin);
+    // 회원가입 중복체크
+    int countByUserId(String userId);
+
+    // 회원 목록
+    List<Admin> selectAllAdmins();
+
+}
