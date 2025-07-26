4ba99d35e286b6ca6175d761a0421355f7b61260
kohyoon <piyoong4@gmail.com>
2025-07-26 22:10:31 +0900

    feat: AdminController에 관리자 등록, 중복 확인, 목록 조회 기능 추가

---
+++ src/main/java/com/project/gym/controller/AdminController.java
@@ -0,0 +1,70 @@
+package com.project.gym.controller;
+
+import com.project.gym.domain.Admin;
+import com.project.gym.service.AdminService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final AdminService adminService;;
+
+    public AdminController (AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+
+    // 관리자 회원가입 폼 호출
+    @GetMapping("/signup")
+    public String showAdminSignUpForm(Model model) {
+        model.addAttribute("admin", new Admin());
+        return "admin/signup";
+    }
+
+    // 관리자 회원가입 처리
+    @PostMapping("/signup")
+    public String processAdminSignUp(@ModelAttribute("admin") Admin admin, Model model) {
+
+        // 아이디 중복여부 확인
+        if(adminService.isUserIdDuplicate(admin.getUserId())) {
+            model.addAttribute("error", "이미 사용중인 아이디입니다.");
+            return "admin/signup";
+        }
+
+        // 비밀번호 확인
+        if(!admin.getPassword().equals(admin.getConfirmPassword())) {
+            model.addAttribute("error", "비밀번호가 일치하지 않습니다.");
+            return "admin/signup";
+        }
+
+        adminService.saveAdmin(admin);
+        return "";
+    }
+
+    // 아이디 중복 확인
+    @GetMapping("/admin/check-userid")
+    @ResponseBody
+    public boolean checkUserId(@RequestParam String userId) {
+        return !adminService.isUserIdDuplicate(userId);
+    }
+
+
+    // 관리자 목록 출력
+    @GetMapping("/list")
+    public String showAdminList(Model model) {
+        List<Admin> adminList = adminService.getAllAdmins();
+        model.addAttribute("adminList", adminList);
+        return "admin/list";
+    }
+
+
+
+
+
+}
