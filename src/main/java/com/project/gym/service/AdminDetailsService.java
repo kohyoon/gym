200f7faeb91455306d407586ef87395f47f6de5d
kohyoon <piyoong4@gmail.com>
2025-07-27 18:54:09 +0900

    feat: 로그인 처리를 위한 AdminDetailsService/Impl 클래스 생성

---
+++ src/main/java/com/project/gym/service/AdminDetailsService.java
@@ -0,0 +1,6 @@
+package com.project.gym.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface AdminDetailsService extends UserDetailsService {
+}
---
+++ src/main/java/com/project/gym/service/AdminDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.project.gym.service;
+
+import com.project.gym.domain.Admin;
+import com.project.gym.domain.AdminDetails;
+import com.project.gym.mapper.AdminMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminDetailsServiceImpl implements AdminDetailsService {
+
+    private final AdminMapper adminMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+
+        Admin admin = adminMapper.selectByUserId(userId);
+        if(admin == null) {
+            throw new UsernameNotFoundException("사용자를 찾을 수 업습니다." + userId);
+        }
+
+        return new AdminDetails(admin);
+    }
+}
