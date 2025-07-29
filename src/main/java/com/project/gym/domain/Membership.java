690bb98432c345b4d2d7a5eca9447abbbff0bec0
kohyoon <piyoong4@gmail.com>
2025-07-29 17:00:07 +0900

    feat: Membership 도메인에 MembershipStatus enum 적용

--- src/main/java/com/project/gym/domain/Membership.java
+++ src/main/java/com/project/gym/domain/Membership.java
@@ -1,5 +1,6 @@
 package com.project.gym.domain;
 
+import com.project.gym.domain.enums.MembershipStatus;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -21,7 +22,7 @@ public class Membership {
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
     private int price;
-    private Integer membershipStatus; // 1이용중 2종료 3환불
+    private MembershipStatus membershipStatus; // 1이용중 2종료 3환불
     private LocalDate createdAt;
     private LocalDate updatedAt;
 
