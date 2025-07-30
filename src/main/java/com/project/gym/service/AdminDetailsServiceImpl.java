ebc82753b3702f60b722fa380590e9754afd248b
kohyoon <piyoong4@gmail.com>
2025-07-30 14:08:56 +0900

    feat: 퇴사자는 로그인 불가하도록 AdminDetailsServiceImpl에 조건 추가

--- src/main/java/com/project/gym/service/AdminDetailsServiceImpl.java
+++ src/main/java/com/project/gym/service/AdminDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.project.gym.service;
 
 import com.project.gym.domain.Admin;
 import com.project.gym.domain.AdminDetails;
+import com.project.gym.domain.enums.AdminRole;
 import com.project.gym.mapper.AdminMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,6 +21,9 @@ public class AdminDetailsServiceImpl implements AdminDetailsService {
         Admin admin = adminMapper.selectByUserId(userId);
         if(admin == null) {
             throw new UsernameNotFoundException("사용자를 찾을 수 업습니다." + userId);
+        }
+        if(admin.getRole() == AdminRole.RESIGNED) {
+            throw new UsernameNotFoundException("퇴사한 회원입니다.");
         }
 
         return new AdminDetails(admin);
