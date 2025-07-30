2ac64f5733cc219fae0fb7149efd209470906036
kohyoon <piyoong4@gmail.com>
2025-07-30 17:11:04 +0900

    refactor: MembershipStatus.SUSPEND → SUSPENDED로 변수명 수정

--- src/main/java/com/project/gym/domain/enums/MembershipStatus.java
+++ src/main/java/com/project/gym/domain/enums/MembershipStatus.java
@@ -2,6 +2,6 @@ package com.project.gym.domain.enums;
 
 public enum MembershipStatus {
     // 1이용중 2정지 3종료 4환불
-    ACTIVE, SUSPEND, FINISHED, REFUND;
+    ACTIVE, SUSPENDED, FINISHED, REFUND;
 
 }
