c2129387bf435054e990b47c4a78577a49f4fae4
kohyoon <piyoong4@gmail.com>
2025-08-01 22:29:48 +0900

    feat: MembershipSuspendMapper에 정지내역 전체 조회 메서드 추가

--- src/main/java/com/project/gym/mapper/MembershipSuspendMapper.java
+++ src/main/java/com/project/gym/mapper/MembershipSuspendMapper.java
@@ -3,13 +3,16 @@ package com.project.gym.mapper;
 import com.project.gym.domain.MembershipSuspendHistory;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface MembershipSuspendMapper {
 
     // 정지 내역 추가
     void insertSuspendHistory(MembershipSuspendHistory suspendHistory);
 
-    // 정지 내역 목록
+    // 정지 내역 - 전체 목록
+    List<MembershipSuspendHistory> selectAllSuspendHistories();
 
     // 정지 내역 상세보기?
 
