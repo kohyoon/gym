138dbaa7d39318d518ef7c125101a3119934c09b
kohyoon <piyoong4@gmail.com>
2025-08-02 17:39:50 +0900

    feat: MembershipsuspendMapper에 membershipId, memberId 기준 조회 메서드 추가

--- src/main/java/com/project/gym/mapper/MembershipSuspendMapper.java
+++ src/main/java/com/project/gym/mapper/MembershipSuspendMapper.java
@@ -14,7 +14,14 @@ public interface MembershipSuspendMapper {
     // 정지 내역 - 전체 목록
     List<MembershipSuspendHistory> selectAllSuspendHistories();
 
-    // 정지 내역 상세보기?
+    // 정지 내역 - MEMBERSHIP_ID 로 조회
+    List<MembershipSuspendHistory> selectSuspendHistoriesByMembershipId(Long membershipId);
+
+    // 정지 내역 - MEMBER_ID 로 조회
+    List<MembershipSuspendHistory> selectSuspendHistoriesByMemberId(Long memberId);
+
+    // 정지 내역 상세보기
+    MembershipSuspendHistory selectSuspendHistoryById(Long suspendId);
 
     // 정지 내역 수정
 
