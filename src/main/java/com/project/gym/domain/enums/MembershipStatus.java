7cc3715a3ee28db8f3f999686eb4ffa581e86c70
kohyoon <piyoong4@gmail.com>
2025-08-04 16:58:43 +0900

    chore: MembershipStatus 주석 수정

--- src/main/java/com/project/gym/domain/enums/MembershipStatus.java
+++ src/main/java/com/project/gym/domain/enums/MembershipStatus.java
@@ -1,7 +1,7 @@
 package com.project.gym.domain.enums;
 
 public enum MembershipStatus {
-    // 1이용중 2정지 3종료 4환불
-    ACTIVE, SUSPENDED, FINISHED, REFUND;
+    // 1이용중 2정지 3환불 4종료
 
+    ACTIVE, SUSPENDED, REFUND, FINISHED;
 }
