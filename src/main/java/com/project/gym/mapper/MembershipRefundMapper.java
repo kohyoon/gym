fd89fa66ce00b8e056ab8ce8c439940bd1459685
kohyoon <piyoong4@gmail.com>
2025-08-06 22:45:07 +0900

    feat: selectKey 사용하여 환불 등록 시 REFUND_ID 사전 조회 및 설정

--- src/main/java/com/project/gym/mapper/MembershipRefundMapper.java
+++ src/main/java/com/project/gym/mapper/MembershipRefundMapper.java
@@ -1,11 +1,7 @@
 package com.project.gym.mapper;
 
 import com.project.gym.domain.MembershipRefundHistory;
-import com.project.gym.domain.MembershipRefundLog;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
 
 @Mapper
 public interface MembershipRefundMapper {
