65652f4aec3541766a597e472c09b2e2792e5787
kohyoon <piyoong4@gmail.com>
2025-08-08 18:47:44 +0900

    feat: RefundLogDTO 생성 (환불 로그용)

---
+++ src/main/java/com/project/gym/dto/membership/refund/RefundLogDTO.java
@@ -0,0 +1,26 @@
+package com.project.gym.dto.membership.refund;
+
+import com.project.gym.domain.MembershipRefundHistory;
+import com.project.gym.domain.enums.ActorRole;
+import com.project.gym.domain.enums.RefundLogType;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+public class RefundLogDTO {
+    //===== 로그 정보 =====//
+    private Long logId;
+    private Long refundId;
+    private RefundLogType logType;
+    private String logDetail;
+    private LocalDate performedAt;
+    private Long performedBy;
+    private ActorRole actorRole;
+
+    //
+    private String performedByName;
+
+
+}
