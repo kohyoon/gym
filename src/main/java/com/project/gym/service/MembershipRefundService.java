bc0f04523812ea9a7e229fecf0a471e2a33425c3
kohyoon <piyoong4@gmail.com>
2025-08-09 17:45:41 +0900

    feat: MembershipRefundService/Impl에 환불 상태를 승인(APPROVED) 처리 -MEMBERSHIP_REFUND_HISTORY 변경 처리 -MEMBERSHIP_REFUND_LOG 추가 메서드 -MEMBERSHIP 테이블 MEMBERSHIP_STATUS 변경

--- src/main/java/com/project/gym/service/MembershipRefundService.java
+++ src/main/java/com/project/gym/service/MembershipRefundService.java
@@ -2,6 +2,7 @@ package com.project.gym.service;
 
 import com.project.gym.domain.MembershipRefundHistory;
 import com.project.gym.domain.enums.ActorRole;
+import com.project.gym.domain.enums.RefundStatus;
 import com.project.gym.dto.membership.refund.RefundDetailDTO;
 import com.project.gym.dto.membership.refund.RefundListDTO;
 
@@ -22,6 +23,9 @@ public interface MembershipRefundService {
     RefundDetailDTO getRefundDetail(Long refundId, ActorRole actorRole);
 
     // 환불 상태 - 검토중(PENDING)으로 수정
-    void markRefundAsPending(Long refundId, Long adminId);
+    void markRefundAsPending(Long refundId, Long adminId, RefundStatus refundStatus);
+
+    // 환불 상태 - 승인(APPROVED)으로 수정
+    void markRefundAsApproved(Long refundId, Long adminId, RefundStatus refundStatus);
 
 }
--- src/main/java/com/project/gym/service/MembershipRefundServiceImpl.java
+++ src/main/java/com/project/gym/service/MembershipRefundServiceImpl.java
@@ -1,18 +1,23 @@
 package com.project.gym.service;
 
+import com.project.gym.domain.Membership;
 import com.project.gym.domain.MembershipRefundHistory;
 import com.project.gym.domain.MembershipRefundLog;
 import com.project.gym.domain.enums.ActorRole;
 import com.project.gym.domain.enums.RefundLogType;
+import com.project.gym.domain.enums.RefundStatus;
 import com.project.gym.dto.membership.refund.RefundDetailDTO;
 import com.project.gym.dto.membership.refund.RefundListDTO;
 import com.project.gym.dto.membership.refund.RefundLogDTO;
+import com.project.gym.mapper.MembershipMapper;
 import com.project.gym.mapper.MembershipRefundLogMapper;
 import com.project.gym.mapper.MembershipRefundMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Service
@@ -21,6 +26,7 @@ public class MembershipRefundServiceImpl implements MembershipRefundService{
 
     private final MembershipRefundMapper refundMapper;
     private final MembershipRefundLogMapper logMapper;
+    private final MembershipMapper membershipMapper;
 
     // 환불 등록
     @Override
@@ -58,22 +64,71 @@ public class MembershipRefundServiceImpl implements MembershipRefundService{
     }
 
     @Override
-    public void markRefundAsPending(Long refundId, Long adminId) {
+    public void markRefundAsPending(Long refundId, Long adminId, RefundStatus refundStatus) {
 
         // refund 정보 저장
         MembershipRefundHistory refund = refundMapper.selectRefundHistoryById(refundId);
-        refund.setReviewedBy(adminId);
 
+        // RefundStatus -> String
+        String status = refundStatus.name();
+
+        refund.setReviewedBy(adminId);
 
         // 로그 정보 저장
         MembershipRefundLog log = MembershipRefundLog.from(
                 refund, RefundLogType.PENDING, ActorRole.ADMIN, refund.getReviewedBy()
         );
+
+
         // 환불 상태 업데이트
         refundMapper.updateRefundStatusToPending(refundId, adminId);
 
         // 로그 추가
         logMapper.insertRefundLog(log);
+    }
+
+    @Override
+    public void markRefundAsApproved(Long refundId, Long adminId, RefundStatus refundStatus) {
+
+        // refund 정보 저장
+        MembershipRefundHistory refund = refundMapper.selectRefundHistoryById(refundId);
+
+        // RefundStatus -> String
+        String status = refundStatus.name();
+
+        refund.setApprovedBy(adminId);
+
+        // 1. 남은 기간 계산
+        Membership membership = membershipMapper.findById(refund.getMembershipId());
+        LocalDate endDate = membership.getEndDate();
+        LocalDate requestDate = refund.getRequestedAt();
+
+        int remainingDays = (int) ChronoUnit.DAYS.between(requestDate, endDate);
+
+        // 2. 이용권 기간, 이용권 가격
+        int periodDays = membership.getPeriodDays();
+        int membershipPrice = membership.getPrice();
+
+        // 3. 환불 금액 = (남은 기간 / 이용권 기간) * 이용권 결제 금액
+        int refundAmount = (int) Math.round(((double) remainingDays / periodDays) * membershipPrice);
+        
+        // 음수인 경우 조건 처리해야함
+
+        refund.setRefundAmount(refundAmount);
+
+        // 로그 정보 저장
+        MembershipRefundLog log = MembershipRefundLog.from(
+                refund, RefundLogType.APPROVED, ActorRole.ADMIN, refund.getApprovedBy()
+        );
+
+        // 환불 상태 업데이트
+        refundMapper.updateRefundStatusToApproved(refundId, adminId, refundAmount);
+
+        // 로그 추가
+        logMapper.insertRefundLog(log);
+
+        // 회원권 테이블 업데이트
+        membershipMapper.refundMembership(refund.getMembershipId(), refund.getApprovedBy());
     }
 
 
