e4d438d5020432cc2fc08ed3a7cd6a1cff8050f0
kohyoon <piyoong4@gmail.com>
2025-08-18 22:55:12 +0900

    refactor: MembershipSuspendController 정지 등록폼 호출, 정지 추가 처리 메서드 수정 (리팩토링 내용 반영)

--- src/main/java/com/project/gym/controller/MembershipSuspendController.java
+++ src/main/java/com/project/gym/controller/MembershipSuspendController.java
@@ -3,8 +3,10 @@ package com.project.gym.controller;
 import com.project.gym.domain.AdminDetails;
 import com.project.gym.domain.Membership;
 import com.project.gym.domain.MembershipSuspendHistory;
+import com.project.gym.dto.membership.suspend.SuspendCreateFormDTO;
 import com.project.gym.service.MembershipService;
 import com.project.gym.service.MembershipSuspendService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -15,53 +17,47 @@ import java.util.List;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/membership/suspend")
 public class MembershipSuspendController {
 
     private final MembershipSuspendService suspendService;
     private final MembershipService membershipService;
 
-    public MembershipSuspendController(MembershipService membershipService,
-                                       MembershipSuspendService suspendService) {
-        this.membershipService = membershipService;
-        this.suspendService = suspendService;
-    }
-
-
-    // 멤버십 정지 폼 호출
+    //===== 멤버십 정지 폼 호출 =====//
     @GetMapping("/register")
-    public String showSuspendForm(@RequestParam Long membershipId, Model model){
-        MembershipSuspendHistory suspendHistory = new MembershipSuspendHistory();
-        suspendHistory.setMembershipId(membershipId);
+    public String showSuspendForm(@RequestParam Long membershipId,
+                                  @AuthenticationPrincipal AdminDetails adminDetails,
+                                  Model model){
+        SuspendCreateFormDTO dto = new SuspendCreateFormDTO();
+        dto.setMembershipId(membershipId);
+        dto.setCreatedBy(adminDetails.getAdmin().getAdminId());
 
         Membership membership = membershipService.findById(membershipId);
 
-        model.addAttribute("suspend", suspendHistory);
+        model.addAttribute("suspend", dto);
         model.addAttribute("membership", membership);
 
         return "membership/suspend/register";
     }
 
-    // 멤버십 정지 추가 처리
+    //===== 멤버십 정지 추가 처리 =====//
     @PostMapping("/register")
-    public String registerSuspend(@ModelAttribute MembershipSuspendHistory suspend,
+    public String registerSuspend(SuspendCreateFormDTO dto,
                                   @AuthenticationPrincipal AdminDetails adminDetails) {
         // 관리자 ID
-        suspend.setCreatedBy(adminDetails.getAdmin().getAdminId());
+        dto.setCreatedBy(adminDetails.getAdmin().getAdminId());
 
         // 정지 이력 등록
-        suspendService.addSuspendHistory(suspend);
+        suspendService.insertSuspendHistory(dto);
 
         Membership membership = new Membership();
-        membership.setMembershipId(suspend.getMembershipId());
-        membership.setExtendedEndDate(suspend.getExtendedEndDate());
+        membership.setMembershipId(dto.getMembershipId());
+        membership.setExtendedEndDate(dto.getExtendedEndDate());
         membership.setUpdatedBy(adminDetails.getAdmin().getAdminId());
-
         membershipService.updateStatusAndExtendedEndDate(membership);
 
-
-
-        return "membership/list";
+        return "redirect:/membership/detail/" + dto.getMembershipId();
     }
 
     // 정지 목록
@@ -89,7 +85,7 @@ public class MembershipSuspendController {
     public String suspendDetail(@PathVariable("id") Long suspendId, Model model){
         MembershipSuspendHistory suspend = suspendService.getSuspendHistoryById(suspendId);
         Membership membership = membershipService.findById(suspend.getMembershipId());
-        suspend.setPeriodDays(membership.getPeriodDays());
+        //suspend.setPeriodDays(membership.getPeriodDays());
 
         model.addAttribute("suspend", suspend);
         model.addAttribute("membership", membership);
