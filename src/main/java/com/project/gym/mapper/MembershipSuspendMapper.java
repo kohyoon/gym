1518807efa1481f1dca3a6add67f98a8713f0658
kohyoon <piyoong4@gmail.com>
2025-08-18 22:52:05 +0900

    feat: MembershipSuspendMapper 수정 및 정지 내역 조회 메서드 추가 - 기존 메서드에 Suspend DTO 반영 - membershipId 기준으로 정지 내역 조회 메서드 추가

--- src/main/java/com/project/gym/mapper/MembershipSuspendMapper.java
+++ src/main/java/com/project/gym/mapper/MembershipSuspendMapper.java
@@ -1,6 +1,8 @@
 package com.project.gym.mapper;
 
 import com.project.gym.domain.MembershipSuspendHistory;
+import com.project.gym.dto.membership.suspend.SuspendCreateFormDTO;
+import com.project.gym.dto.membership.suspend.SuspendListDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -9,13 +11,14 @@ import java.util.List;
 public interface MembershipSuspendMapper {
 
     // 정지 내역 추가
-    void insertSuspendHistory(MembershipSuspendHistory suspendHistory);
+    void insertSuspendHistory(SuspendCreateFormDTO dto);
 
     // 정지 내역 - 전체 목록
     List<MembershipSuspendHistory> selectAllSuspendHistories();
 
     // 정지 내역 - MEMBERSHIP_ID 로 조회
     List<MembershipSuspendHistory> selectSuspendHistoriesByMembershipId(Long membershipId);
+    List<SuspendListDTO> selectSuspendListDTOByMembershipId(Long membershipId);
 
     // 정지 내역 - MEMBER_ID 로 조회
     List<MembershipSuspendHistory> selectSuspendHistoriesByMemberId(Long memberId);
