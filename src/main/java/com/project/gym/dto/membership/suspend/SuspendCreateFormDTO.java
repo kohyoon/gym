24f57e8efc575d0d2c11187c5888e86ae2a666df
kohyoon <piyoong4@gmail.com>
2025-08-18 22:50:27 +0900

    feat: 정지 등록폼 작성을 위한 SuspendCreateFormDTO 생성

---
+++ src/main/java/com/project/gym/dto/membership/suspend/SuspendCreateFormDTO.java
@@ -0,0 +1,32 @@
+package com.project.gym.dto.membership.suspend;
+
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Data
+public class SuspendCreateFormDTO {
+
+    private Long suspendId;
+    private Long membershipId;
+    private String membershipType; // PT, pilates, gym
+
+    private Long memberId;
+    private String memberName;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate suspendStartDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate suspendEndDate;
+    private String suspendReason;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate createdAt;
+    private Long createdBy; // 생성한 관리자ID
+    private String createdByName;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate extendedEndDate;
+
+}
