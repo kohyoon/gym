c7196326bf56d52d62302ee60f7e60fc11a7487c
kohyoon <piyoong4@gmail.com>
2025-08-20 17:32:23 +0900

    refactor(security): 통합 로그인 페이지 적용에 따른 SecurityConfig 로그인 경로 수정

--- src/main/java/com/project/gym/config/SecurityConfig.java
+++ src/main/java/com/project/gym/config/SecurityConfig.java
@@ -31,20 +31,20 @@ public class SecurityConfig {
         http
                 .securityMatcher("/admin/**", "/auth/admin_login", "/auth/logout")
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/admin_login", "/admin/signup").permitAll()
+                        .requestMatchers("/auth/login", "/admin/signup").permitAll()
                         .anyRequest().hasAnyRole("ADMIN")
                 )
                 .formLogin(form -> form
-                        .loginPage("/auth/admin_login")
+                        .loginPage("/auth/login")
                         .loginProcessingUrl("/auth/admin_login")
-                        .usernameParameter("username")
-                        .passwordParameter("password")
+                        .usernameParameter("adminId")
+                        .passwordParameter("adminPw")
                         .defaultSuccessUrl("/admin/adminPage", true) // 로그인 성공 시 이동 경로
-                        .failureUrl("/auth/admin_login?error")
+                        .failureUrl("/auth/login?error")
                 )
                 .logout(logout -> logout
                         .logoutUrl("/auth/logout") // 사용자가 요청할 로그아웃 경로
-                        .logoutSuccessUrl("/auth/admin_login?logout") // 로그아웃 후 리디렉션할 경로
+                        .logoutSuccessUrl("/auth/login?logout") // 로그아웃 후 리디렉션할 경로
                         .invalidateHttpSession(true) // 세션 무효화
                         .deleteCookies("JSESSIONID") // 쿠키 삭제
                         .permitAll()
@@ -62,20 +62,21 @@ public class SecurityConfig {
         http.
                 securityMatcher("/member/**", "/auth/member_login", "/auth/logout", "/membership/**")
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/member/check-loginId", "/member/check-email", "/auth/member_login", "/member/signup").permitAll()
+                        .requestMatchers("/member/check-loginId", "/member/check-email",
+                                "/auth/login", "/member/signup").permitAll()
                         .anyRequest().hasAnyRole("MEMBER", "ADMIN")
                 )
                 .formLogin(form -> form
-                        .loginPage("/auth/member_login")
+                        .loginPage("/auth/login")
                         .loginProcessingUrl("/auth/member_login")
-                        .usernameParameter("username")
-                        .passwordParameter("password")
+                        .usernameParameter("memberId")
+                        .passwordParameter("memberPw")
                         .defaultSuccessUrl("/member/mypage", true)
-                        .failureUrl("/auth/member_login?error")
+                        .failureUrl("/auth/login?error")
                 )
                 .logout(logout -> logout
-                        .logoutUrl("/auth/logout")
-                        .logoutSuccessUrl("/auth/member_login?logout")
+                        .logoutUrl("/auth/login")
+                        .logoutSuccessUrl("/auth/login?logout")
                         .invalidateHttpSession(true) // 세션 무효화
                         .deleteCookies("JSESSIONID") // 쿠키 삭제
                         .permitAll()
