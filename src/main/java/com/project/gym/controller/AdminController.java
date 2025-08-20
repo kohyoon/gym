1c0d88b0ac374c649ff940b45cf52eca644ee323
kohyoon <piyoong4@gmail.com>
2025-08-20 17:33:04 +0900

    refactor: AdminController 관리자 목록 조회 메서드에 검색 조건 반영 feat: AdminController 관리자 정보 수정 메서드 추가

--- src/main/java/com/project/gym/controller/AdminController.java
+++ src/main/java/com/project/gym/controller/AdminController.java
@@ -1,14 +1,13 @@
 package com.project.gym.controller;
 
+import com.project.gym.common.PageResult;
 import com.project.gym.domain.Admin;
-import com.project.gym.domain.AdminDetails;
+import com.project.gym.dto.admin.AdminListDTO;
+import com.project.gym.dto.admin.AdminSearchCriteria;
 import com.project.gym.service.AdminService;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -55,15 +54,17 @@ public class AdminController {
         return !adminService.isUserIdDuplicate(userId);
     }
 
+    //===== 회원 목록 조회 =====//
+    @GetMapping("/search")
+    public String showAdminSearchLists(@ModelAttribute("cond") AdminSearchCriteria criteria, Model model) {
+        PageResult<AdminListDTO> page = adminService.getAdminByCriteria(criteria);
+        model.addAttribute("page", page);
+        model.addAttribute("adminList", page.content());
+        model.addAttribute("cond", criteria);
 
-    // 관리자 목록 출력
-    @GetMapping("/list")
-    public String showAdminList(Model model, @AuthenticationPrincipal AdminDetails adminDetails) {
-        List<Admin> adminList = adminService.getAllAdmins();
-        model.addAttribute("adminList", adminList);
-        model.addAttribute("adminName", adminDetails.getAdmin().getAdminName());
         return "admin/list";
     }
+
 
     // 회원 정보 수정 폼 호출
     @GetMapping("/edit/{id}")
@@ -75,7 +76,17 @@ public class AdminController {
         return "admin/edit";
     }
 
-    // 회원 정보 수정 처리
+    // 정보 수정 처리
+    @PostMapping("/edit")
+    public String processAdminEdit(Admin admin) {
+        System.out.println("****** admin: " + admin);
+
+        adminService.updateAdmin(admin);
+
+        return "redirect:/admin/adminPage";
+    }
+
+    // 관리자 퇴사 처리
     @PostMapping("/resign/{id}")
     public String resignAdmin(@PathVariable("id") Long adminId) {
 
