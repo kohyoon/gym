856187b5812e101a9df1ff72a9b9f3e7a68439de
kohyoon <piyoong4@gmail.com>
2025-08-20 17:27:58 +0900

    refactor: AuthController 로그인 페이지 매핑을 통합 페이지로 수정

--- src/main/java/com/project/gym/controller/AuthController.java
+++ src/main/java/com/project/gym/controller/AuthController.java
@@ -2,25 +2,20 @@ package com.project.gym.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
 
-    @GetMapping("/admin_login")
-    public String showAdminLoginForm() {
-        return "auth/admin_login";
-    }
-
     @GetMapping("/logout-success")
     public String logoutPage() {
-        return "logout"; //
+        return "logout";
     }
 
-    @GetMapping("/member_login")
-    public String showMemberLoginForm() { return "auth/member_login"; }
-
+    @GetMapping("/login")
+    public String showMainPageLogin() {
+        return "auth/login";
+    }
 
 }
